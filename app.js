8f80f06c38bc8c5eea4164e68dd83c9785963fc0
Sian Culligan <SianCulligan@gmail.com>
2019-09-13 18:29:50 -0700

    images no longer duplicate & event listener stops after 25 clicks

--- app.js
+++ app.js
@@ -4,11 +4,12 @@
 var votesRemaining = 25;
 var picturesEl = document.getElementById('pictures');
 var resultsEl = document.getElementById('results');
-var picOneEl = document.getElementById('picOne');
-var picTwoEl = document.getElementById('picTwo');
-var picThreeEl = document.getElementById('picThree');
+var picOneEl = document.getElementById('pic-one');
+var picTwoEl = document.getElementById('pic-two');
+var picThreeEl = document.getElementById('pic-three');
 var containerEl = document.getElementById('pictureContainer');
 var allProductArr = [];
+var recentIndex = [];
 
 function Product (filePath, productName){
     this.alt = productName;
@@ -19,30 +20,51 @@ function Product (filePath, productName){
 
     allProductArr.push(this);
 }
-// pulls random image from the constructor
+// pulls random image from the constructor array
 function imageGenerator(){
+    
+    while(recentIndex.length > 6){
+        recentIndex.shift();
+    }
+    console.log( 'recent index', recentIndex);
+
     var index = random(allProductArr.length);
+
+    while(recentIndex.includes(index)) {
+        index = random(allProductArr.length);
+    }
     picOneEl.src = allProductArr[index].src;
     picOneEl.alt = allProductArr[index].alt;
     picOneEl.title = allProductArr[index].title;
-
     allProductArr[index].views++;
+    recentIndex.push(index);
+
 
     var indexTwo = random(allProductArr.length);
-    while(indexTwo === index){
+
+    while(recentIndex.includes(indexTwo)) {
         indexTwo = random(allProductArr.length);
     }
     picTwoEl.src = allProductArr[indexTwo].src;
     picTwoEl.alt = allProductArr[indexTwo].alt;
     picTwoEl.title = allProductArr[indexTwo].title;
-   
+    allProductArr[indexTwo].views++;
+    recentIndex.push(indexTwo);
+
+
     var indexThree = random(allProductArr.length);
-    while(indexThree === index|indexThree === indexTwo){
-        indexTwo = random(allProductArr.length);
+
+    while(recentIndex.includes(indexThree)) {
+        indexThree = random(allProductArr.length);
     }
     picThreeEl.src = allProductArr[indexThree].src;
     picThreeEl.alt = allProductArr[indexThree].alt;
     picThreeEl.title = allProductArr[indexThree].title;
+    allProductArr[indexThree].views++;
+    recentIndex.push(indexThree);
+
+   
+
 }
 
 // Generate a random number for index position in the image generating function
@@ -71,6 +93,25 @@ new Product('img/usb.gif','Moving tentacle USB');
 new Product('img/water-can.jpg','Impossible watering can');
 new Product('img/wine-glass.jpg','Really annoying wine glass');
 
+
+function renderMostPopular(){
+
+    //once votes run out, 
+    var mostPopular;
+    var clicks = 0;
+
+    for(var i = 0; i < allProductArr.length; i++){
+      if(allProductArr[i].votes > clicks){
+        clicks = allProductArr[i].votes;
+        mostPopular = allProductArr[i];
+      }
+    }
+
+    var h2El = document.createElement('h2');
+    h2El.textContent = `The most popular product is ${mostPopular.productName} with ${mostPopular.votes} votes.`;
+    resultsEl.appendChild(h2El);
+  }
+
 function handleClick(event){
     var clickedProduct = event.target.title;
     for(var i = 0; i < allProductArr.length; i++){
@@ -79,8 +120,16 @@ function handleClick(event){
         }
     }
     imageGenerator();
+    votesRemaining--;
+
+    if(votesRemaining === 0){
+        containerEl.removeEventListener('click', handleClick);
+        renderMostPopular();
+    }
 }
+
+
+
 
 containerEl.addEventListener('click', handleClick);
 imageGenerator();
-
